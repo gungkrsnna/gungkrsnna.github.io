cbe6b8e22e7e090c77c2362b87fa4882534da2c2
gungkrsnna <thegunggg@gmail.com>
2023-07-05 13:31:45 +0800

    initial commit

--- Mewah/js/sutotext.js
+++
@@ -1,7 +0,0 @@
-const selectButton = document.querySelector('.button-open-text');
-const autoText = document.querySelector('.autoText');
-
-selectButton.addEventListener('click', function(){
-      autoText.focus();
-      autoText.select();
-});
